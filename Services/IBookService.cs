88ab0491ac6d1d88e99c6e8c58e096a88ff1345d
xconor <d00261104@student.dkit.ie>
2025-10-24 00:23:17 +0100

    Define IBookService interface for book operations

---
+++ Services/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using BookMediaDiscoverer.Models;
+
+namespace BookMediaDiscoverer.Services
+{
+    public interface IBookService
+    {
+        Task<List<Book>> SearchBooksAsync(string query);
+        Task<Book> GetBookDetailsAsync(string bookId);
+    }
+}
